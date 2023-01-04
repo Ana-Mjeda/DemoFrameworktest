74248d9aa01331c13c34389385b1fa7141d74b81
ana.mjeda <anamjeda@gmail.com>
2023-01-04 14:12:31 +0100

    small refactor

--- src/test/java/nopCommerceTests/TemplateTest.java
+++ src/test/java/nopCommerceTests/TemplateTest.java
@@ -6,19 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.net.URL;
-
-public class templateTest {
+public class TemplateTest {
 
     public TemplatePage templatePage;
 
     @BeforeClass
-    public void beforeClass(){
+    public void beforeClass() {
         WebDriver driver = BrowserFactory.startBrowser("www.google.com");
         templatePage = new TemplatePage(driver);
     }
+
     @Test
-    public void Step1(){
+    public void Step1() {
         templatePage.clickLoginButton("hello", "world")
                 .clickLogin();
     }
