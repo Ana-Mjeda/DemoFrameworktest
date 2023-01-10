fa89256c2f62324a2d58dd2b765d576b6171e8e5
ana.mjeda <anamjeda@gmail.com>
2023-01-10 15:54:20 +0100

    NEW TASK

--- src/main/java/nopCommerce/TemplatePage.java
+++ src/main/java/nopCommerce/TemplatePage.java
@@ -2,13 +2,7 @@ package nopCommerce;
 
 import Base.BaseUI;
 import Base.BrowserFactory;
-import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-import java.util.List;
 
 public class TemplatePage extends BrowserFactory {
     BaseUI ui;
