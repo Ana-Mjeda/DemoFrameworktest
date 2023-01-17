1f9d75d5cf443c3eb8edd02126730bf998cf3d1d
ana.mjeda <anamjeda@gmail.com>
2023-01-17 09:20:05 +0100

    fixed annotations on HomePage

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -1,31 +1,37 @@
 package page;
 
-import nopCommerce.TemplatePage;
-import org.openqa.selenium.By;
+import base.BaseUI;
+import base.BrowserFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class HomePage extends TemplatePage {
+public class HomePage extends BrowserFactory {
+    BaseUI baseUI;
+    @FindBy(className = "ico-register")
+    WebElement registerButton;
+    @FindBy(className = "ico-login")
+    WebElement loginButton;
+    @FindBy(className = "ico-logout")
+    WebElement logoutButton;
 
     public HomePage(WebDriver driver) {
         super(driver);
+        baseUI = new BaseUI(driver);
     }
 
     public void clickRegisterButton() {
-        WebElement registerButton = driver.findElement(By.className("ico-register"));
-        registerButton.click();
+        baseUI.click(registerButton);
         System.out.println("Register button clicked");
     }
 
     public void clickLoginButton() {
-        WebElement loginButton = driver.findElement(By.className("ico-login"));
-        loginButton.click();
+        baseUI.click(loginButton);
         System.out.println("Login button clicked");
     }
 
     public void clickLogoutButton() {
-        WebElement logoutButton = driver.findElement(By.className("ico-logout"));
-        logoutButton.click();
+        baseUI.click(logoutButton);
         System.out.println("Logout button clicked");
     }
 }
